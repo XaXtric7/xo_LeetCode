0ddf16535ef0ea2f959536e9bb3ec777ad3c034f
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-02-23 00:10:21 +0530

    23-02-25_leet

---
+++ 26. Remove_Duplicates_from_Sorted_Array/26_Remove_Duplicate(optimal).cpp
@@ -0,0 +1,18 @@
+// Time complexity -> O(n), Space complexity -> O(1)..
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int j = 1;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
---
+++ 27. Remove_Element/27_Remove_Element.cpp
@@ -0,0 +1,18 @@
+// Time complexity -> O(n), Space complexity -> O(1)..
+class Solution
+{
+public:
+    int removeElement(vector<int> &nums, int val)
+    {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};

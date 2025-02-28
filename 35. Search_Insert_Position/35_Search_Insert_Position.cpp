05316a29c80c651d54265fdc386cc3f02ab5cf0f
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-02-28 20:12:07 +0530

    35_using_binary_search

---
+++ 35. Search_Insert_Position/35_Search_Insert_Position.cpp
@@ -0,0 +1,32 @@
+// Using Binary Search
+// time Complexity -> O(log n)
+// Space Complexity -> O(1)
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int n = nums.size() - 1;
+        int left = 0;
+        int right = n;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};

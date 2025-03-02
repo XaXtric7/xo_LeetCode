f6e08f37cd9b4e7b35e9a4b4400c3ca53edd15fd
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-03-02 19:17:44 +0530

    2570_merging_2x_2D_arrays

---
+++ Daily Challenge/2570_Merge_Two_2D_Arrays_by_Summing_Values.cpp
@@ -0,0 +1,46 @@
+// Time Complexit -> O(n + m)
+// Space Complexity -> O(n + m)
+
+class Solution
+{
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>> &nums1, vector<vector<int>> &nums2)
+    {
+        vector<vector<int>> nums3;
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size())
+        {
+            if (nums1[i][0] == nums2[j][0])
+            {
+                nums3.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            }
+            else if (nums1[i][0] < nums2[j][0])
+            {
+                nums3.push_back(nums1[i]);
+                i++;
+            }
+            else
+            {
+                nums3.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        while (i < nums1.size())
+        {
+            nums3.push_back(nums1[i]);
+            i++;
+        }
+
+        while (j < nums2.size())
+        {
+            nums3.push_back(nums2[j]);
+            j++;
+        }
+
+        return nums3;
+    }
+};

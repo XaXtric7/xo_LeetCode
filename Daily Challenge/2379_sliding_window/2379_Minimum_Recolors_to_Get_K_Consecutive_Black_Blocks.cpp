cca2e399c6a73ca038bc42eadcdb098fdeebf9c1
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-03-08 16:03:34 +0530

    2379_sliding_window

--- Daily Challenge/2379_sliding_window/2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
+++ Daily Challenge/2379_sliding_window/2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
@@ -3,33 +3,25 @@ class Solution
 public:
     int minimumRecolors(string blocks, int k)
     {
-        int n = blocks.length();
-        int min_recolors = k; // Maximum recolors needed in worst case
+        int n = blocks.length(); // stores the lenght of the string
+        int recolor = INT_MAX;   // initialized to the max integer value
+        int black_cnt = 0;
 
-        // Count initial 'W's in the first window
-        int white_count = 0;
-        for (int i = 0; i < k; i++)
+        for (int i = 0; i < n; i++)
         {
-            if (blocks[i] == 'W')
-                white_count++;
+            if (blocks[i] == 'B')
+            { // skip the white and only focus on the black
+                black_cnt++;
+            }
+            if (i >= k - 1)
+            { // can form a valid window
+                recolor = min(k - black_cnt, recolor);
+                if (blocks[i - k + 1] == 'B')
+                {
+                    black_cnt--;
+                }
+            }
         }
-
-        min_recolors = white_count;
-
-        // Slide the window across the string
-        for (int i = k; i < n; i++)
-        {
-            // Remove leftmost element of previous window
-            if (blocks[i - k] == 'W')
-                white_count--;
-            // Add rightmost element of current window
-            if (blocks[i] == 'W')
-                white_count++;
-
-            // Update the minimum number of recolors needed
-            min_recolors = min(min_recolors, white_count); // mininimum of (intital white_counts in the begining, white_count in the window)
-        }
-
-        return min_recolors;
+        return recolor;
     }
 };

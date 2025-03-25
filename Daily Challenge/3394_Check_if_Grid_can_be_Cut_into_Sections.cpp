f0fd139bf4d4f2e91af3d73765f8b8afda952982
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-03-26 00:00:37 +0530

    3394_chek_if_grid_can_cut_into_sec

---
+++ Daily Challenge/3394_Check_if_Grid_can_be_Cut_into_Sections.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+private:
+    bool canCut(vector<vector<int>> &rectangles, int axis)
+    {
+        sort(rectangles.begin(), rectangles.end(),
+             [axis](vector<int> &a, vector<int> &b)
+             { return a[axis] < b[axis]; });
+
+        int cuts = 0, previousEnd = -1;
+
+        for (auto &rect : rectangles)
+        {
+            int start = rect[axis], end = rect[axis + 2];
+
+            if (start >= previousEnd)
+                cuts++;
+            previousEnd = max(previousEnd, end);
+            if (cuts >= 3)
+                return true;
+        }
+
+        return false;
+    }
+
+public:
+    bool checkValidCuts(int n, vector<vector<int>> &rectangles)
+    {
+        return canCut(rectangles, 0) || canCut(rectangles, 1);
+    }
+};

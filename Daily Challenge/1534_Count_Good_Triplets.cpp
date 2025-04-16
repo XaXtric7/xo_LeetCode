24f1ea2f5d3776963e17a9f9984bd352c20d5b75
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-04-16 13:15:27 +0530

    1534_Count_Good_Triplets

---
+++ Daily Challenge/1534_Count_Good_Triplets.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int countGoodTriplets(vector<int> &arr, int a, int b, int c)
+    {
+        int good = 0;
+        for (int i = 0; i < arr.size() - 2; i++)
+        {
+            for (int k = 2; k < arr.size(); k++)
+            {
+                if (abs(arr[k] - arr[i]) > c)
+                {
+                    continue;
+                }
+                for (int j = i + 1; j < k; j++)
+                {
+                    if (abs(arr[j] - arr[i]) <= a && abs(arr[j] - arr[k]) <= b)
+                    {
+                        good++;
+                    }
+                }
+            }
+        }
+        return good;
+    }
+};

477a3d3f98d924d422c54966b4fa2643d737083e
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-04-20 22:49:06 +0530

    724_Find_Pivot_Index

---
+++ LeetCode 75/724_Find_Pivot_Index.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+        int rsum = accumulate(nums.begin(), nums.end(), 0); // sum of all the ele. in the container
+        int lsum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            rsum -= nums[i];
+            if (lsum == rsum)
+            {
+                return i;
+            }
+            lsum += nums[i];
+        }
+        return -1;
+    }
+};

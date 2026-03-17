e8402a32818e88573058edf76816659c2564529e
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-03-17 11:59:19 +0530

    01. Two Sum

--- 01. Two_Sum/1_Two_Sum.cpp
+++ 01. Two_Sum/1_Two_Sum.cpp
@@ -3,6 +3,27 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        vector<int> ans;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         for (int i = 0; i < nums.size(); i++)
         {
             for (int j = i + 1; j < nums.size(); j++)

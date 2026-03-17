e05b3fb7031de255aab0935918a4a96270a8e2f9
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-03-18 02:10:21 +0530

    49. Group Anagrams

--- NeetCode_roadmap/Arrays_Hashing/49_Group_Anagrams.cpp
+++ NeetCode_roadmap/Arrays_Hashing/49_Group_Anagrams.cpp
@@ -1,3 +1,5 @@
+// My solution..
+
 class Solution
 {
 public:
@@ -16,5 +18,75 @@ public:
             anagrams.push_back(p.second);
         }
         return anagrams;
+    }
+};
+
+// Using oriority Queue...
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> freq;
+
+        for (int x : nums)
+        {
+            freq[x]++;
+        }
+
+        // min heap: {frequency, element}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        for (auto it : freq)
+        {
+            pq.push({it.second, it.first});
+            if (pq.size() > k)
+            {
+                pq.pop(); // remove smallest frequency
+            }
+        }
+
+        vector<int> ans;
+        while (!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
+
+// Using Bucket Sort....
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> freq;
+
+        for (int x : nums)
+            freq[x]++;
+
+        vector<vector<int>> bucket(nums.size() + 1);
+
+        for (auto it : freq)
+        {
+            bucket[it.second].push_back(it.first);
+        }
+
+        vector<int> ans;
+
+        for (int i = nums.size(); i >= 0 && ans.size() < k; i--)
+        {
+            for (int num : bucket[i])
+            {
+                ans.push_back(num);
+                if (ans.size() == k)
+                    break;
+            }
+        }
+
+        return ans;
     }
 };

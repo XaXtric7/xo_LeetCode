6619233921afc0c60aa626212d0c51c1076da6f7
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-03-18 15:52:10 +0530

    1929 Concatenation of Array

---
+++ NeetCode_roadmap/Arrays_Hashing/1929_Concatenation_of_Array.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        vector<int> ans;
+        int n = nums.size();
+        ans.insert(ans.end(), nums.begin(), nums.end());
+        for (int i = 0; i < n; i++)
+        {
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};

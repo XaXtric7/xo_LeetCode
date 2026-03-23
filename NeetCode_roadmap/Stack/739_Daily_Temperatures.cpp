735b658370d88f2a792470682870d4dee2ee4368
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-03-24 05:24:05 +0530

    739. Daily Temperatures

---
+++ NeetCode_roadmap/Stack/739_Daily_Temperatures.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        int n = temperatures.size();
+        stack<int> st;
+        vector<int> ans(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()])
+            {
+                int prev = st.top();
+                st.pop();
+                ans[prev] = i - prev;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};

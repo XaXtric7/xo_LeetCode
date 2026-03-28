b4df81aa32d73ac63c1e4d1ea4a27765621bae35
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-03-28 10:36:12 +0530

    875. Koko Eating Bananas

---
+++ NeetCode_roadmap/Binary Search/875_Koko_Eating_Bananas.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+private:
+    bool canFinish(vector<int> &piles, int h, int k)
+    {
+        long long hours = 0;
+        for (int pile : piles)
+        {
+            hours += pile / k;
+            if (pile % k != 0)
+                hours++;
+            if (hours > h)
+                return false;
+        }
+        return hours <= h;
+    }
+
+public:
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+        int ans = right;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (canFinish(piles, h, mid))
+            {
+                ans = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};

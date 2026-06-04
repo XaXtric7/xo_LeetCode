b1c322ed0b6cfd6371a5142b0f94a93591ce8c3f
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-06-04 13:27:04 +0530

    Fixed issues

--- NeetCode_roadmap/4. Binary Search/875_Koko_Eating_Bananas.cpp
+++ NeetCode_roadmap/4. Binary Search/875_Koko_Eating_Bananas.cpp
@@ -1,3 +1,27 @@
+// Naive approach..
+// find the max element in the array and term it as maxSpeed
+// from 1 to maxSpeed use a for loop and for each speed find the req time to finish the bananas
+
+int kokoEat(vector<int> &arr, int k)
+{
+    int mx = *max_element(arr.begin(), arr.end());
+    for (int speed = 1; speed <= mx; speed++)
+    {
+        long long reqTime = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            reqTime += (arr[i] + speed - 1) / speed;
+        }
+        if (reqTime <= k)
+        {
+            return speed;
+        }
+    }
+    return mx;
+}
+
+// Optimal solution....
+
 class Solution
 {
 private:

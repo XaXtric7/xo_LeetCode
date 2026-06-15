6ff33c9416b8220af7fa17cc91ad4f085e1df05a
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-06-15 17:26:16 +0530

    Reverse Nodes in K grp

---
+++ NeetCode_roadmap/6. Linked List/25_Reverse_Nodes_in_k_Group.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode *groupPrev = &dummy;
+
+        while (true)
+        {
+            // Find the kth node
+            ListNode *kth = groupPrev;
+            for (int i = 0; i < k && kth != nullptr; i++)
+            {
+                kth = kth->next;
+            }
+
+            if (kth == nullptr)
+                break;
+
+            ListNode *groupNext = kth->next;
+
+            // Reverse current group
+            ListNode *prev = groupNext;
+            ListNode *curr = groupPrev->next;
+
+            while (curr != groupNext)
+            {
+                ListNode *temp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            // Reconnect
+            ListNode *temp = groupPrev->next;
+            groupPrev->next = kth;
+            groupPrev = temp;
+        }
+
+        return dummy.next;
+    }
+};

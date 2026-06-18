8990d69de78b59ed015c2305ce266e8c89c9587d
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-06-18 08:38:12 +0530

    Build tree from inorder & preorder

--- NeetCode_roadmap/7. Trees/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
+++ NeetCode_roadmap/7. Trees/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -51,3 +51,46 @@ public:
         return buidIT(mp, inorder, preorder, preIndex, 0, inorder.size() - 1);
     }
 };
+
+// O(n), O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    TreeNode *buidIT(unordered_map<int, int> &mp, vector<int> &inorder, vector<int> &preorder, int &preIndex, int left, int right)
+    {
+        if (left > right)
+        {
+            return nullptr;
+        }
+        int rootVal = preorder[preIndex++];
+        TreeNode *root = new TreeNode(rootVal);
+        int pivot = mp[rootVal];
+        root->left = buidIT(mp, inorder, preorder, preIndex, left, pivot - 1);
+        root->right = buidIT(mp, inorder, preorder, preIndex, pivot + 1, right);
+        return root;
+    }
+
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+    {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < inorder.size(); i++)
+        {
+            mp[inorder[i]] = i;
+        }
+        int preIndex = 0;
+        return buidIT(mp, inorder, preorder, preIndex, 0, inorder.size() - 1);
+    }
+};

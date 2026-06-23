aea3e589e897f2eaef4023ce0654664f546c7f1a
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-06-23 16:58:00 +0530

    Remove Duplicate II

---
+++ NeetCode_roadmap/2. Two Pointers/80_Remove_Duplicates_from_Sorted_Array_II.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.size() <= 2)
+        {
+            return nums.size();
+        }
+
+        int insertIndex = 2;
+        for (int i = 2; i < nums.size(); i++)
+        {
+            int count = 0;
+            if (nums[i] != nums[insertIndex - 2])
+            {
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+};

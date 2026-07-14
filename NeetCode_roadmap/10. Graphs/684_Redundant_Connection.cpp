8a1c55837733fb4fa569de7f4cc5c8a4b7f1e03e
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-07-14 19:45:15 +0530

    Redundant Connection (using DSU)

--- NeetCode_roadmap/10. Graphs/684_Redundant_Connection.cpp
+++ NeetCode_roadmap/10. Graphs/684_Redundant_Connection.cpp
@@ -1,51 +1,65 @@
-class DisjointSet{
+class DisjointSet
+{
     vector<int> parent, size;
+
 public:
-    DisjointSet(int n){
+    DisjointSet(int n)
+    {
         parent.resize(n + 1);
         size.resize(n + 1, 1);
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++)
+        {
             parent[i] = i;
         }
     }
-    int findParent(int node){
-        if(node == parent[node]){
+    int findParent(int node)
+    {
+        if (node == parent[node])
+        {
             return node;
         }
         return parent[node] = findParent(parent[node]);
     }
 
-    void unionBySize(int u, int v){
+    void unionBySize(int u, int v)
+    {
         int ulp_u = findParent(u);
         int ulp_v = findParent(v);
 
-        if(ulp_u == ulp_v){
+        if (ulp_u == ulp_v)
+        {
             return;
         }
-        if(size[ulp_u] < size[ulp_v]){
+        if (size[ulp_u] < size[ulp_v])
+        {
             parent[ulp_u] = ulp_v;
             size[ulp_v] += size[ulp_u];
         }
-        else{
+        else
+        {
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
     }
 };
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+    vector<int> findRedundantConnection(vector<vector<int>> &edges)
+    {
         int n = edges.size();
 
         DisjointSet ds(n);
 
-        for(auto &edge : edges){
+        for (auto &edge : edges)
+        {
             int u = edge[0];
             int v = edge[1];
 
-            if(ds.findParent(u) == ds.findParent(v)){
+            if (ds.findParent(u) == ds.findParent(v))
+            {
                 return edge;
             }
             ds.unionBySize(u, v);
